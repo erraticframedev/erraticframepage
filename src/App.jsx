373cd304f9cb59bca9f3fd40004fbc8b1f7ed256
erraticframedev <erraticframe@gmail.com>
2024-07-27 14:45:24 +0200

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, Fragment, useEffect } from 'react'
 import './App.css'
+import { createApi } from "unsplash-js"
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+const api = createApi({
+  accessKey: fff
+})
+
+
+
+function StaggeredGrid(data, columns = 2) {
+  
+
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='staggeredGrid'>
+    {data.map(a => {
+    const { id, user, urls } = a
+    
+    return ( <img key={id} src={urls.regular} /> )
+   }
+    )}
+    </div>
     </>
   )
+}
+
+function App() {
+  const [data, setPhotosResponse] = useState(null);
+
+  api.users.getPhotos
+  useEffect(() => {
+    api.users
+      .getPhotos({
+        username: "erraticframe",
+        orientation: "landscape",
+        stats: true
+      })
+      .then(result => {
+        setPhotosResponse(result);
+      })
+      .catch(() => {
+        console.log("something went wrong!");
+      });
+  }, []);
+
+  if (data === null) {
+    return <div>Loading...</div>;
+  } else if (data.errors) {
+    return (
+      <div>
+        <div>{data.errors[0]}</div>
+        <div>PS: Make sure to set your access token!</div>
+      </div>
+    );
+  } else {
+    return (
+      <StaggeredGrid data = {data.response.results} />
+    )
+  }
 }
 
 export default App
