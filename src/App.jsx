c7f88ef8fdd325e216b640e0ea8fd8eb95879438
erraticframedev <erraticframe@gmail.com>
2024-07-28 19:25:17 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import { useState, Fragment, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import { createApi } from "unsplash-js"
 import { Splide, SplideSlide } from "@splidejs/react-splide"
+import * as THREE from 'three';
+import { Canvas, useFrame } from '@react-three/fiber'
 
 
 const api = createApi({
   accessKey: "e1uyUwS-ZeVJucBPfEHg1tKtwYpH5apYxBCWZI1fBkY"
 })
 
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (
+    ref.current.rotation.x += delta,
+    ref.current.rotation.y += delta
+    ))
 
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+      >
+      
+      <sphereGeometry args={[1.5, 16, 16]}  />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} stencilWrite={true} wireframe={true}   />
+  
+      
+      
+    </mesh>
+  )
+}
 
 function StaggeredGrid({photos}, columns = 2) {
   return(
@@ -55,7 +87,7 @@ function App() {
     api.users
       .getPhotos({
         username: "erraticframe",
-        orientation: "landscape",
+      
         stats: true
       })
       .then(result => {
@@ -77,9 +109,20 @@ function App() {
     );
   } else {
     return (
+      <>
+       <Canvas>
+    <ambientLight intensity={Math.PI / 2} />
+    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+    <Box position={[-1.2, 0, 0]} />
+    <Box position={[1.2, 0, 0]} />
+  </Canvas>
+      
       <StaggeredGrid 
       photos = {unsplashResult.response.results}
        columns = {2} />
+
+</>
     )
   }
 }
